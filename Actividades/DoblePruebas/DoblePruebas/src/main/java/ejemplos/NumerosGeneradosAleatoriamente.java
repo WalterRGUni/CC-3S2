07acee7f0a0420b426a961cda0c1cba484c3e0dd
Walter Rivera Gambini <wriverag@uni.pe>
2023-06-05 12:24:37 -0500

    se realizó actividad doblePruebas

---
+++ Actividades/DoblePruebas/DoblePruebas/src/main/java/ejemplos/NumerosGeneradosAleatoriamente.java
@@ -0,0 +1,13 @@
+package ejemplos;
+
+import ejemplos.NumerosAleatorios;
+
+import java.util.random.RandomGenerator;
+
+public class NumerosGeneradosAleatoriamente implements NumerosAleatorios {
+    private final RandomGenerator rnd = RandomGenerator.getDefault();
+    @Override
+    public int nextInt(int upperBoundExclusive) {
+        return rnd.nextInt(upperBoundExclusive);
+    }
+}
